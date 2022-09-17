aa5feda6909ba704498d686c01ba39383c85c80e
Rakshit <rakshit087@gmail.com>
2022-09-17 17:49:21 +0530

    Added Bottom Navigation

--- src/App.js
+++ src/App.js
@@ -1,22 +1,13 @@
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {Provider as PaperProvider} from 'react-native-paper';
-import {createMaterialBottomTabNavigator} from '@react-navigation/material-bottom-tabs';
-import {Dashboard} from './pages/Dashboard';
-import {Statistics} from './pages/Statistics';
-import {Settings} from './pages/Settings';
-
-const Tab = createMaterialBottomTabNavigator();
+import {NavigationBar} from './layouts/NavigationBar';
 
 const App = () => {
   return (
     <PaperProvider>
       <NavigationContainer>
-        <Tab.Navigator>
-          <Tab.Screen name="Home" component={Dashboard} />
-          <Tab.Screen name="Statistics" component={Statistics} />
-          <Tab.Screen name="Settings" component={Settings} />
-        </Tab.Navigator>
+        <NavigationBar />
       </NavigationContainer>
     </PaperProvider>
   );
