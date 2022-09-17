6f5a7877ef401d76659e185b3ab83a0ced7cec31
Rakshit <rakshit087@gmail.com>
2022-09-18 00:50:10 +0530

    Centered Statistics and Settings

--- src/pages/Settings.js
+++ src/pages/Settings.js
@@ -1,11 +1,26 @@
 import React from 'react';
-import {View} from 'react-native';
+import {View, StyleSheet} from 'react-native';
 import {Text} from 'react-native-paper';
 
 export const Settings = () => {
   return (
-    <View>
+    <View style={style.container}>
       <Text>Settings</Text>
     </View>
   );
 };
+
+const style = StyleSheet.create({
+  container: {
+    display: 'flex',
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  card: {
+    backgroundColor: '#ffffff',
+    height: '30%',
+    width: '60%',
+    borderRadius: 12,
+  },
+});
