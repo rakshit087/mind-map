06b2c931ed653dd18feee5b3cdd215fa9d814737
Rakshit <rakshit087@gmail.com>
2022-09-18 01:34:50 +0530

    Added Heading

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {Provider as PaperProvider} from 'react-native-paper';
 import {NavigationBar} from './layouts/NavigationBar';
+import {TopBar} from './layouts/TopBar';
 
 const App = () => {
   return (
     <PaperProvider>
       <NavigationContainer>
+        <TopBar />
         <NavigationBar />
       </NavigationContainer>
     </PaperProvider>
