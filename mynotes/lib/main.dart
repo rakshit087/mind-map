7d1e9c3378a1d4164b0fa91952007762d4b45270
Rakshit <rakshit087@gmail.com>
2022-10-09 23:47:00 +0530

    Added named routes

--- mynotes/lib/main.dart
+++ mynotes/lib/main.dart
@@ -2,6 +2,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:mynotes/firebase_options.dart';
+import 'package:mynotes/views/login_view.dart';
 import 'package:mynotes/views/register_view.dart';
 import 'package:mynotes/views/verify_email.dart';
 
@@ -12,12 +13,17 @@ void main() {
     theme: ThemeData(
       primarySwatch: Colors.blue,
     ),
-    home: const Routes(),
+    home: const LoginView(),
+    routes: {
+      '/login': (context) => const LoginView(),
+      '/register': (context) => const RegisterView(),
+      '/home': (context) => const HomeView()
+    },
   ));
 }
 
-class Routes extends StatelessWidget {
-  const Routes({super.key});
+class HomeView extends StatelessWidget {
+  const HomeView({super.key});
 
   @override
   Widget build(BuildContext context) {
