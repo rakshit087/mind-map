73dd99214f3cf3bb02683fab91d88cc25b684d4b
Rakshit <rakshit087@gmail.com>
2022-10-11 23:25:06 +0530

    Added NotesView

---
+++ mynotes/lib/views/notes_view.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+enum MenuAction { logout }
+
+class NotesView extends StatefulWidget {
+  const NotesView({super.key});
+
+  @override
+  State<NotesView> createState() => _NotesViewState();
+}
+
+class _NotesViewState extends State<NotesView> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(title: const Text("Notes")), body: const Text("Notes"));
+  }
+}
