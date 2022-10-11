d51323de70db371b87d09a0b3ed24cbec37056c7
Rakshit <rakshit087@gmail.com>
2022-10-12 00:00:44 +0530

    added logout button

--- mynotes/lib/views/notes_view.dart
+++ mynotes/lib/views/notes_view.dart
@@ -1,3 +1,5 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 enum MenuAction { logout }
@@ -13,6 +15,51 @@ class _NotesViewState extends State<NotesView> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(title: const Text("Notes")), body: const Text("Notes"));
+        appBar: AppBar(title: const Text("Notes"), actions: [
+          PopupMenuButton<MenuAction>(
+            itemBuilder: (context) {
+              return const [
+                PopupMenuItem(
+                  child: Text("Logout"),
+                  value: MenuAction.logout,
+                )
+              ];
+            },
+            onSelected: (value) async {
+              switch (value) {
+                case MenuAction.logout:
+                  bool shouldLogout = await showLogoutAlert(context);
+                  if (shouldLogout) {
+                    await FirebaseAuth.instance.signOut();
+                    Navigator.of(context)
+                        .pushNamedAndRemoveUntil('/login/', (_) => false);
+                  }
+                  break;
+              }
+            },
+          )
+        ]),
+        body: const Text("Notes"));
   }
+}
+
+Future<bool> showLogoutAlert(BuildContext context) {
+  return showDialog<bool>(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: const Text("Logout"),
+          content: const Text("Are you sure you want to logout?"),
+          actions: [
+            TextButton(
+              child: const Text("Cancel"),
+              onPressed: () => Navigator.of(context).pop(false),
+            ),
+            TextButton(
+              child: const Text("Logout"),
+              onPressed: () => Navigator.of(context).pop(true),
+            ),
+          ],
+        );
+      }).then((value) => value ?? false);
 }
