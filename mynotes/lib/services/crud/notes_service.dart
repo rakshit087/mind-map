ec4e867df698f11e4e80d2410bf8bbabc1b18a32
Rakshit <rakshit087@gmail.com>
2022-11-03 23:20:32 +0530

    Added DatabaseUser

--- mynotes/lib/services/crud/notes_service.dart
+++ mynotes/lib/services/crud/notes_service.dart
@@ -1,7 +1,13 @@
+import 'package:flutter/material.dart';
 import 'package:sqflite/sqflite.dart';
-import 'package:path/path.dart';
-import 'package:path_provider/path_provider.dart';
+import 'package:path/path.dart' show join;
+import 'package:path_provider/path_provider.dart'
+    show getApplicationDocumentsDirectory;
 
+const idColumn = "id";
+const emailColumn = "email";
+
+@immutable
 class DatabaseUser {
   final int id;
   final String email;
@@ -10,4 +16,16 @@ class DatabaseUser {
     required this.id,
     required this.email,
   });
+
+  DatabaseUser.fromRow(Map<String, Object?> map)
+      : id = map[idColumn] as int,
+        email = map[emailColumn] as String;
+
+  @override
+  bool operator ==(covariant DatabaseUser other) {
+    return id == other.id;
+  }
+
+  @override
+  int get hashCode => id;
 }
