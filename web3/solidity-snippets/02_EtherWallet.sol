a65e3ab39ee727ca0ba6e53791197c28a3244157
Rakshit <rakshit087@gmail.com>
2022-11-12 23:56:22 +0530

    web3: VerifySignature

--- web3/solidity-snippets/02_EtherWallet.sol
+++ web3/solidity-snippets/02_EtherWallet.sol
@@ -1,6 +1,15 @@
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.8.9;
 
+// recieve vs fallback
+/* 
+    fallback runs when a function is called that does not exist
+    recieve runs when ether is sent to the contract
+    if call data is not empty, fallback is called
+    else if both are defined, recieve takes precedence
+    if recieve is not defined, fallback is called
+*/
+
 contract EtherWallet {
     address public immutable owner;
 
@@ -18,6 +27,12 @@ contract EtherWallet {
     function withdraw(uint256 _amount) external onlyOwner {
         require(address(this).balance >= _amount, "not enough funds");
         payable(msg.sender).transfer(_amount);
+    }
+
+    function withdrawUsingCall(uint256 _amount) external onlyOwner {
+        require(address(this).balance >= _amount, "not enough funds");
+        (bool success, ) = msg.sender.call{value: _amount}("");
+        require(success, "failed to send ether");
     }
 
     function getBalance() public view returns (uint256) {
