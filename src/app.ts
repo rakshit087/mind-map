179c4137b641b715ae50a6104f8e8c5040b737dc
Rakshit Arora <rakshit087@gmail.com>
2023-02-17 01:16:22 +0530

    (web/backend): added logger

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,13 @@
 import express from "express";
 import config from "config";
 import connect from "./utils/connect";
+import logger from "./utils/logger";
 
 const port = config.get<number>("port");
 const app = express();
 
 app.listen(port, async () => {
-  console.log("Server is running on port 1337");
+  logger.info(`Server is running on port ${port}`);
   await connect();
 });
 
