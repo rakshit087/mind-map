abec9539cb5c4524425c940b1f3f5cab93ff9232
Rakshit Arora <rakshit087@gmail.com>
2023-02-17 01:09:21 +0530

    (web/backend): connected to mongodb

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,13 @@
 import express from "express";
 import config from "config";
+import connect from "./utils/connect";
 
 const port = config.get<number>("port");
 const app = express();
 
-app.listen(port, () => {
+app.listen(port, async () => {
   console.log("Server is running on port 1337");
+  await connect();
 });
 
 console.log("hello world");
