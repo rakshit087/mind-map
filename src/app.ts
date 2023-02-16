d429e0a155b48d9b4d46ea6df60fe81f46fb2302
Rakshit Arora <rakshit087@gmail.com>
2023-02-17 01:24:54 +0530

    (web/backend): added / route

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import express from "express";
 import config from "config";
 import connect from "./utils/connect";
 import logger from "./utils/logger";
+import routes from "./routes";
 
 const port = config.get<number>("port");
 const app = express();
@@ -9,6 +10,7 @@ const app = express();
 app.listen(port, async () => {
   logger.info(`Server is running on port ${port}`);
   await connect();
+  routes(app);
 });
 
 console.log("hello world");
