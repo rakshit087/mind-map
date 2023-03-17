4083e9964275bcc8767d0d2bb26f857966b9c8f6
Rakshit Arora <rakshit087@gmail.com>
2023-03-17 14:51:12 +0530

    added user controller

--- web/backend/src/routes.ts
+++ web/backend/src/routes.ts
@@ -1,9 +1,11 @@
 import { Express, Request, Response } from "express";
+import { createUserHandler } from "./controller/user.controller";
 
 function routes(app: Express) {
   app.get("/", (req: Request, res: Response) => {
     res.send("Hello World!");
   });
+  app.post("/api/users", createUserHandler);
 }
 
 export default routes;
