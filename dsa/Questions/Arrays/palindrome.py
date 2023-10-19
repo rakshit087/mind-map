89588f3d3ce98ba985913873d4ac5f8c00d3b361
Rakshit <rakshit087@gmail.com>
2023-10-19 21:57:31 +0530

    removed dsa and flutter

--- dsa/Questions/Arrays/palindrome.py
+++
@@ -1,6 +0,0 @@
-class Solution:
-    def isPlaindrome(self, S):
-        # code here
-        if(S==S[::-1]):
-            return 1
-        return 0
--- dsa/Questions/Arrays/subset.py
+++
@@ -1,7 +0,0 @@
-def isSubset( a1, a2, n, m):
-    for i in a2:
-        if(i not in a1):
-            return 'No'
-        else:
-            continue
-    return 'Yes'
